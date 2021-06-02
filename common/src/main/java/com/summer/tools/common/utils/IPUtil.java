a195583fa6b9852da6f110c641cb6654bb03b12c
wangfenglei <wangfenglei@eversec.cn>
2021-06-02 17:53:12 +0800

    添加百度ip定位功能-待完善

--- common/src/main/java/com/summer/tools/common/utils/IPUtil.java
+++ common/src/main/java/com/summer/tools/common/utils/IPUtil.java
@@ -1,23 +1,32 @@
 package com.summer.tools.common.utils;
 
-
-import net.minidev.json.JSONObject;
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import javax.servlet.http.HttpServletRequest;
-import java.io.ByteArrayOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.text.MessageFormat;
 
 public class IPUtil {
 
-
     private static Logger LOGGER = LoggerFactory.getLogger(IPUtil.class);
 
+    /**
+     * 百度应用访问key,申请并激活百度开发者账号,创建个应用就可以获得
+     */
+    private static final String AK = "Y8125nlttvjMzqrK9n4mVEnNYnmccfoq";
+    /**
+     * 百度定位服务接口,开发文档 https://lbsyun.baidu.com/index.php?title=webapi/ip-api
+     */
+    private static final String LOCATION_SERVICE_URL = "http://api.map.baidu.com/location/ip?ak={0}&coor=bd09ll&ip={1}";
+
+    /**
+     * 获取ip地址
+     */
     public static String getIpAddr(HttpServletRequest request){
 
         String ip = request.getHeader("x-forwarded-for");
@@ -34,74 +43,34 @@ public class IPUtil {
         return ip;
     }
 
-
     /**
-     * 
-     * 获取IP+IP所属地址
-     */
-    public static String getIpBelongAddress(HttpServletRequest request){
-
-        String ip = getIpAddr(request);
-        String belongIp = getIPbelongAddress(ip);
-
-        return ip + belongIp;
-    }
-    /**
-     * 
      * 获取IP所属地址
      */
-    public static String getIPbelongAddress(String ip){
-
-        String ipAddress = "[]";
-        try{
-            //淘宝提供的服务地址
-//            String context = call("http://ip.taobao.com/service/getIpInfo.php?ip="+ip);
-//            JSONObject fromObject = JSONObject.fromObject(context);
-//            String code = fromObject.getString("code");
-//            if(code.equals("0")){
-//                JSONObject jsonObject = fromObject.getJSONObject("data");
-//                ipAddress =  "["+jsonObject.get("country")+"/" +jsonObject.get("city")+"]";
-//            }
-        }catch(Exception e){
-        LOGGER.error("获取IP所属地址出错",e);
-            e.printStackTrace();
-        }
-        return ipAddress;
-    }
-    /**
-     * 获取Ip所属地址
-     */
-    public static String call( String urlStr ){
-
+    public static String getLocationByIP(String strIP) {
         try {
-
-            URL url = new URL(urlStr);
-            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-
-            httpCon.setConnectTimeout(3000);
-            httpCon.setDoInput(true);
-            httpCon.setRequestMethod("GET");
-
-            int code = httpCon.getResponseCode();
-
-            if(code == 200){
-                return FileUtil.streamToSting(httpCon.getInputStream(), null);
+            URL url = new URL(MessageFormat.format(LOCATION_SERVICE_URL, AK, strIP));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream(), StandardCharsets.UTF_8));
+            String line;
+            StringBuilder result = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
             }
-        } catch (Exception e) {
-            LOGGER.error("获取IP所属地址出错",e);
-            e.printStackTrace();
+            reader.close();
+            String ipAddr = result.toString();
+            JSONObject obj1= JSONObject.parseObject(ipAddr);
+            if("0".equals(obj1.get("status").toString())){
+                JSONObject obj2= JSONObject.parseObject(obj1.get("content").toString());
+                JSONObject obj3= JSONObject.parseObject(obj2.get("address_detail").toString());
+                return obj3.get("city").toString();
+            }else{
+                return "读取失败";
+            }
+        } catch (IOException e) {
+            return "读取失败";
         }
-        return null;
     }
 
     public static void main(String[] args) {
-
-
-        String context = call("https://blog.csdn.net/yo_yolv/article/details/51646482");
-
-        Map<String, Object> fromObject = JsonUtil.parseMap(context, Object.class);
-        Map<String, Object> data = JsonUtil.parseMap(JsonUtil.stringify(fromObject.get("data")), Object.class);;
-        System.out.println(fromObject);
-        System.err.println(data.get("city"));
+        System.out.println(getLocationByIP("1.15.74.240"));
     }
 }
